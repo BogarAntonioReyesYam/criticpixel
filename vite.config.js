61726a6eb28209dcbee93e070514b2de869fa48f
BogarAntonioReyesYam <00906389@red.unid.mx>
2026-06-04 01:20:17 -0600

    Fix: Re-ajustada base a /criticpixel/ y despliegue final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // ESTO HACE QUE TODAS LAS RUTAS SEAN RELATIVAS (PORTABILIDAD TOTAL)
+  base: '/criticpixel/', // REQUERIDO PARA REPOSITORIOS QUE NO SON .github.io PUROS
 })

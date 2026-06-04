2ac5db7d3033fb7a287578573ea7a6a04c79866e
BogarAntonioReyesYam <00906389@red.unid.mx>
2026-06-04 01:26:26 -0600

    Debug: Añadido indicador de carga y vuelto a rutas relativas

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/criticpixel/', // REQUERIDO PARA REPOSITORIOS QUE NO SON .github.io PUROS
+  base: './', // VOLVEMOS A RUTA RELATIVA: ES LA MÁS SEGURA PARA TODO
 })
